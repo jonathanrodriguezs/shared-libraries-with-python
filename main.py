6f425b0eae8f4f82691832501070ad129a9f2efa
Jonathan Rodriguez <jonathan.rodriguez.sanchez@u-erre.mx>
2020-09-04 22:28:25 -0500

    feat: add simple example in main.py

--- main.py
+++ main.py
@@ -1,21 +1,8 @@
-import ctypes
-import glob
-import sys
-
-def load_asm_math():
-    # Find the shared library
-    libfile = glob.glob('build/*/asm_math*.so')[0]
-
-    # Open the shared library .so
-    asm_math = ctypes.CDLL(libfile)
-
-    # Tell Python interpreter the argument and result types of function sum
-    asm_math.sum.restype = ctypes.c_int
-    asm_math.sum.argtypes = [ctypes.c_int, ctypes.c_int]
-
-    return asm_math
+from setup_libraries import load_asm_math
 
 if __name__ == "__main__":
     asm_math = load_asm_math()
-    a, b = int(sys.argv[1]), int(sys.argv[2])
-    asm_math.sum(a, b)
+    asm_math.sum(10, 100)
+
+    fib = asm_math.fibonacci(20)
+    print("Fibonacci {} is {}".format(20, fib))
