63adcd1dbf452c394a249d410a653ef15fb069c3
Jonathan Rodriguez <jonathan.rodriguez.sanchez@u-erre.mx>
2020-09-04 22:26:51 -0500

    chore: apply formatter

--- asm_math.cpp
+++ asm_math.cpp
@@ -1,14 +1,28 @@
-#include<iostream>
+#include <iostream>
 
-extern "C"
+extern "C" int sum(int a, int b)
+{
+  int result;
+  __asm__("addl %%ebx, %%eax;"
+          : "=a"(result)
+          : "a"(a), "b"(b));
+  printf("%d + %d = %d\n", a, b, result);
+  return result;
+}
 
 /**
  * Sum two numbers with ASM
- * @return sum
+ * @param n last fibonacci number
+ * @return fibonnaci sequence as array
  */
-int sum(int a, int b) {
-    int result;
-    __asm__("addl %%ebx, %%eax;" : "=a" (result) : "a" (a) , "b" (b));
-    printf("%d + %d = %d\n", a, b, result);
-    return result;
+extern "C" int fibonacci(int n)
+{
+  int f[n + 2];
+  f[0] = 0;
+  f[1] = 1;
+
+  for (int i = 2; i <= n; i++)
+    f[i] = f[i - 1] + f[i - 2];
+
+  return f[n];
 }
