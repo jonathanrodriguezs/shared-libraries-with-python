5d248479d46b3fbe7c2c63d0050ebcecbb352e21
Jonathan Rodriguez <jonathan.rodriguez.sanchez@u-erre.mx>
2020-09-05 00:33:45 -0500

    feat: larger range of values with double and long long instead int in asm_math.cpp

--- asm_math.cpp
+++ asm_math.cpp
@@ -1,25 +1,26 @@
 #include <iostream>
 
 /** Sum two numbers with ASM */
-extern "C" int sum(int a, int b)
+extern "C" long long sum(long long a, long long b)
 {
-  int result;
+  long long result;
   __asm__("addl %%ebx, %%eax;"
           : "=a"(result)
           : "a"(a), "b"(b));
-  printf("%d + %d = %d\n", a, b, result);
   return result;
 }
 
 /** Retrieve the n fibonacci number in the sequence */
-extern "C" int fibonacci(int n)
+extern "C" double fibonacci(int n)
 {
-  int f[n + 2];
-  f[0] = 0;
-  f[1] = 1;
-
+  double a = 0, b = 1, c;
+  if (n == 0)
+    return a;
   for (int i = 2; i <= n; i++)
-    f[i] = f[i - 1] + f[i - 2];
-
-  return f[n];
+  {
+    c = a + b;
+    a = b;
+    b = c;
+  }
+  return b;
 }
